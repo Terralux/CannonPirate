aacba45fc44597e40edefd3afcf931f8f92b4108
AwesomePirate <Holtmannpatrick@gmail.com>
2017-02-07 18:13:27 +0100

    LevelManagerStart
    LevelDataScripts made with help from Light

---
+++ Cannon Pirate/Assets/Scripts/LevelDataScritableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelDataScritableObject {
+
+	public int maxStarRatingLimit;
+	public int halfStarRatingLimit;
+
+	public int treasureCount;
+	public Texture2D levelImage;
+
+	public SceneField levelAsset;
+
+}
